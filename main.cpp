45bf7f8fc71d6a3ff69aeba34ab114033a8188da
KotaNch <ilacihancov01@gmail.com>
2026-04-23 09:46:06 +0500

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+
+
+using namespace std;
+
+#ifdef _WIN32
+#include <windows.h>
